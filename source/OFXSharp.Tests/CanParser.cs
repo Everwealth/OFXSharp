3be5273ccf5b89b70459babaff115db95df7577a
Sam Thwaites <sam@thwaitesy.com>
2016-06-12 22:52:58 +1000

    Fixes for new xml header but old SGML format.. bit of a hack but it works. Added a few more sample files.

--- source/OFXSharp.Tests/CanParser.cs
+++ source/OFXSharp.Tests/CanParser.cs
@@ -16,6 +16,8 @@ namespace OFXSharp.Tests
         {
             var parser = new OFXDocumentParser();
             var ofxDocument = parser.Import(new FileStream(@"itau.ofx", FileMode.Open));
+            Assert.AreEqual(3, ofxDocument.Transactions.Count);
+            Assert.AreEqual(-666.66M, ofxDocument.Transactions.First().Amount);
         }
 
         [TestMethod]
@@ -23,6 +25,37 @@ namespace OFXSharp.Tests
         {
             var parser = new OFXDocumentParser();
             var ofxDocument = parser.Import(new FileStream(@"santander.ofx", FileMode.Open));
+
+            Assert.IsTrue(ofxDocument.Transactions.Count > 0);
+
+        }
+
+
+        [TestMethod]
+        public void CanParserANZ()
+        {
+            var parser = new OFXDocumentParser();
+            var ofxDocument = parser.Import(new FileStream(@"ANZ.ofx", FileMode.Open));
+
+            Assert.IsTrue(ofxDocument.Transactions.Count > 0);
+        }
+
+        [TestMethod]
+        public void CanParserSample1()
+        {
+            var parser = new OFXDocumentParser();
+            var ofxDocument = parser.Import(new FileStream(@"Sample1.ofx", FileMode.Open));
+
+            Assert.IsTrue(ofxDocument.Transactions.Count > 0);
+        }
+
+        [TestMethod]
+        public void CanParserSample2()
+        {
+            var parser = new OFXDocumentParser();
+            var ofxDocument = parser.Import(new FileStream(@"Sample2.ofx", FileMode.Open));
+
+            Assert.IsTrue(ofxDocument.Transactions.Count > 0);
         }
     }
 }
